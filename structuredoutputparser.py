5a2a3cb51e8fb5b0c7ebbc277d043a3d56273d6e
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 15:41:30 +0530

    sturcturedoutputparser using schema

---
+++ structuredoutputparser.py
@@ -0,0 +1,48 @@
+import os
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+
+load_dotenv()
+
+hf_token = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+    huggingfacehub_api_token=hf_token
+)
+
+model = ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give 3 fact about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+prompt = template.invoke({'topic':'black hole'})
+
+result = model.invoke(prompt)
+
+final_result = parser.parse(result.content)
+
+print(final_result)
+
+# {
+# 'fact_1': 'Black holes are regions of spacetime where gravity is so strong that nothing, not even light, can escape.', 
+# 'fact_2': 'They are formed when extremely massive stars collapse at the end of their life cycle.',
+# 'fact_3': 'Despite their immense gravity, black holes have extremely small diameters. Some black holes are only a few miles across.'
+# }
+
+
